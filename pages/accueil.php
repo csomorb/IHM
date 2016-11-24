597f7feb70f7cda20dad3603a6a01e49d920fad2
garnesson <pgns@laposte.net>
2016-11-24 10:48:28 +0000

    modification

--- pages/accueil.php
+++ pages/accueil.php
@@ -1,8 +1,20 @@
 <div class="container">
     <div class="row">
         <div class="col s2">
+            <!-- menu latéral gauche page d'accueil -->
             <div class="card-panel">
-                ici menu
+                <img class="responsive-img circle" src="img/jean.png">
+               Bonjour <?php echo $_SESSION['user']; ?>
+                <hr/>
+                <!-- affichage des groupes -->
+                 <span class="grey-text darken-3-text">Raccourcis</span>
+                <ul>
+                    <?php if ($_SESSION['user'] == "jean"){ ?>
+                        <li><a href="#!" class="title-menu-accueil collection-item">Master WIC</a></li>
+                        <li><a href="#!" class="title-menu-accueil collection-item active">Master WIC</a></li>
+                        <li><a href="#!" class="title-menu-accueil collection-item">Les anciens camarades</a></li>
+                    <?php } ?>
+                </ul>
             </div>
         </div>
         <div class="col s8">
@@ -10,9 +22,6 @@
                 <div class="col s12">
                    <div class="card-panel">
                         <!-- message pouvant etre poster par l'utilisateur -->
-                        <?php
-                            echo "bienvenue". $_SESSION['user']; 
-                        ?>
                         <div class="input-field">
                             <label for="textarea1">Exprimer-vous</label>
                             <textarea id="textarea1" class="materialize-textarea"></textarea>
