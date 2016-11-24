4ce5e1401cfb3da0050dd2808c4a3e90721f1724
garnesson <pgns@laposte.net>
2016-11-24 09:45:56 +0000

    Création de la page d'accueil : -ajout col menu + MODIF MENU

--- menu.php
+++ menu.php
@@ -1,14 +1,39 @@
+
 <div class="navbar-fixed">
     <nav>
       <div class="nav-wrapper blue lighten-4">
-        <a href="/accueil" class="brand-logo">Profil</a>
-        <ul class="right hide-on-med-and-down">
-          <li><a href="/messagerie">Messagerie</a></li>
-          <li><a href="/groupes">Groupes</a></li>
-          <li><a href="/ami">Amis</a></li>
-          <li><a href="/evenement">Evénements</a></li>
-          <li><a href="/logout_action.php">Logout</a></li>
-        </ul>
-      </div>
-    </nav>
+        <div class="container">
+          
+          <!--<div class="search-wrapper card">
+            <input id="search"><i class="material-icons">search</i>
+            <div class="search-results"></div>
+          </div>-->
+          
+          <div class="row">
+            <div class="col s6">
+              
+              
+              <form>
+                <div class="input-field">
+                  <input id="search" type="search" required>
+                  <label for="search"><i class="material-icons">search</i></label>
+                  <i class="material-icons"></i>
+                </div>
+              </form>
+              
+              
+            </div>
+            <div class="col s6">
+              <ul class="right hide-on-med-and-down">
+                <li><a href="/messagerie">Messagerie</a></li>
+                <li><a href="/groupes">Groupes</a></li>
+                <li><a href="/ami">Amis</a></li>
+                <li><a href="/evenement">Evénements</a></li>
+                <li><a href="/logout_action.php">Logout</a></li>
+              </ul>
+            </div>
+          
+        </div>
+      </nav>
+    </div>
   </div>
--- pages/accueil.php
+++ pages/accueil.php
@@ -1,5 +1,44 @@
-<?php
-echo "bienvenue". $_SESSION['user']; 
-
-
-?>
+<div class="container">
+    <div class="row">
+        <div class="col s2">
+            <div class="card-panel">
+                ici menu
+            </div>
+        </div>
+        <div class="col s8">
+            <div class="row">
+                <div class="col s12">
+                   <div class="card-panel">
+                        <!-- message pouvant etre poster par l'utilisateur -->
+                        <?php
+                            echo "bienvenue". $_SESSION['user']; 
+                        ?>
+                        <div class="input-field">
+                            <label for="textarea1">Exprimer-vous</label>
+                            <textarea id="textarea1" class="materialize-textarea"></textarea>
+                           
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12">
+                    <div class="card-panel">
+                        <!--affichage des derniers message de groupes ' a voir avec barnabas-->
+                        flux de données
+                    </div>       
+                </div>
+            </div>
+        </div>
+        <div class="col s2">
+            <!-- liste des évenement en cours -->
+            <div class="card-panel">
+                Liste des évenement
+            </div>
+        </div>
+    </div>
+</div>
+    
+    
+    
+    
+    
+    
