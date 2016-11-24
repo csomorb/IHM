e337bc1f98c48939a75eb1e70e48495cce49ed6a
garnesson <pgns@laposte.net>
2016-11-24 12:09:20 +0000

    modif accueil + css

--- pages/accueil.php
+++ pages/accueil.php
@@ -7,7 +7,7 @@
                Bonjour <?php echo $_SESSION['user']; ?>
                 <hr/>
                 <!-- affichage des groupes -->
-                 <span class="grey-text darken-3-text">Raccourcis</span>
+                <span class="grey-text darken-3-text">RACCOURCIS</span>
                 <ul>
                     <?php if ($_SESSION['user'] == "jean"){ ?>
                         <li><a href="#!" class="title-menu-accueil collection-item">Master WIC</a></li>
@@ -15,17 +15,24 @@
                         <li><a href="#!" class="title-menu-accueil collection-item">Les anciens camarades</a></li>
                     <?php } ?>
                 </ul>
+                <hr/>
+                <span class="grey-text darken-3-text">PARCOURIR</span>
+                <ul>
+                    <li><a href="#!" class="/groupe">Groupes</a></li>
+                    <li><a href="#!" class="/page">Pages</a></li>
+                    <li><a href="#!" class="/evenement">Évenements</a></li>
+                </ul>
             </div>
         </div>
-        <div class="col s8">
+        <div class="col s7">
             <div class="row">
                 <div class="col s12">
                    <div class="card-panel">
                         <!-- message pouvant etre poster par l'utilisateur -->
                         <div class="input-field">
-                            <label for="textarea1">Exprimer-vous</label>
+                           <label for="textarea1">Exprimer-vous</label>
                             <textarea id="textarea1" class="materialize-textarea"></textarea>
-                           
+                            
                         </div>
                     </div>
                 </div>
@@ -33,21 +40,64 @@
                     <div class="card-panel">
                         <!--affichage des derniers message de groupes ' a voir avec barnabas-->
                         flux de données
+                            
                     </div>       
                 </div>
             </div>
         </div>
-        <div class="col s2">
+        <div class="col s3">
             <!-- liste des évenement en cours -->
-            <div class="card-panel">
-                Liste des évenement
+            <div class="card">
+                <div class="row">
+                    <div class="col s12">
+                        <center><h4 class="header">Évènement</h4></center>
+                        <!--<div class="card">
+                            <span class="card-title"><center>Évènement</center></span>
+                        </div>-->
+                    </div>
+                    <div class="col s12">
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="https://scontent.xx.fbcdn.net/v/t1.0-0/p240x240/15085697_698488080302752_4043143901866369599_n.jpg?oh=a29b233cf81a3b7f1e564d93afb38810&oe=58D21DCD">
+                            </div>
+                            <div class="card-content">
+                                <span class="title-card-evenement-accueil">Université Grenoble Alpes</span>
+                                <p class="text-card-evenement-accueil">Quand nos basketteuses élite font le + beau "Kass" de l'Univ de Nice...70 - 51 !<br/><a>#MBLU</a> <a>#UGAvsNice</a></p>
+                                <a class="right">Voir</a><br/>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col s12">
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="http://www.univ-grenoble-alpes.fr/medias/photo/trump_1479286588628-jpg">
+                            </div>
+                            <div class="card-content">
+                                <span class="title-card-evenement-accueil">Université Grenoble Alpes</span>
+                                <p class="text-card-evenement-accueil">Trump Président, et après ? 45è président des USA... A J+7, tu te questionnes ? 4 experts UGA croisent ...</a></p>
+                                <a class="right">Voir</a><br/>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col s12">
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="https://scontent.xx.fbcdn.net/v/t1.0-9/14184276_10201855880394363_2286520047715810801_n.jpg?oh=b0e82242ffd041edaaef5efa44d877a5&oe=58B97333">
+                                <span class="card-title">Master WIC</span>
+                            </div>
+                            <div class="card-content">
+                                <span class="title-card-evenement-accueil">Master WIC <span class="grey-text darken-1-text">Promo 2016</span></span>
+                                <p class="text-card-evenement-accueil">On est au bshm ou a l'imag cette aprem ?</p>
+                                <a> 2 Réponses </a><a class="right">Voir</a>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
             </div>
         </div>
     </div>
 </div>
-    
-    
-    
-    
-    
     
