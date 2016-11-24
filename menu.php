689f81fc71a52c61a99cffe0c930bc75845b88a1
garnesson <pgns@laposte.net>
2016-11-24 09:49:36 +0000

    ajout profil dans le menu

--- menu.php
+++ menu.php
@@ -8,11 +8,8 @@
             <input id="search"><i class="material-icons">search</i>
             <div class="search-results"></div>
           </div>-->
-          
           <div class="row">
             <div class="col s6">
-              
-              
               <form>
                 <div class="input-field">
                   <input id="search" type="search" required>
@@ -20,11 +17,10 @@
                   <i class="material-icons"></i>
                 </div>
               </form>
-              
-              
             </div>
             <div class="col s6">
               <ul class="right hide-on-med-and-down">
+                <li><a href="/accueil"><?php echo $_SESSION['user']; ?></a></li>
                 <li><a href="/messagerie">Messagerie</a></li>
                 <li><a href="/groupes">Groupes</a></li>
                 <li><a href="/ami">Amis</a></li>
@@ -32,7 +28,6 @@
                 <li><a href="/logout_action.php">Logout</a></li>
               </ul>
             </div>
-          
         </div>
       </nav>
     </div>
