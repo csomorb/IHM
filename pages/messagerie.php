8b39513ce9eb1d6e5369389798a7b1bbfa589002
garnesson <pgns@laposte.net>
2016-11-25 15:52:17 +0000

    modif sur les messages

---
+++ pages/messagerie.php
@@ -0,0 +1,38 @@
+<div class="container">
+    <div class="row">
+        <div class="col s2">
+            <!-- menu latéral gauche page d'accueil -->
+            <div class="card-panel">
+                liste des amis
+            </div>
+        </div>
+        <div class="col s7">
+            <div class="row">
+                <div class="col s12">
+                   <div class="card-panel">
+                        <!-- message pouvant etre poster par l'utilisateur -->
+                        <div class="input-field">
+                           <label for="textarea1">Nouveau message</label>
+                            <textarea id="textarea1" class="materialize-textarea"></textarea>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12">
+                    <div class="card-panel">
+                        <!--affichage des derniers message de groupes ' a voir avec barnabas-->
+                        Liste des anciens messages
+                            
+                    </div>       
+                </div>
+            </div>
+        </div>
+        <div class="col s3">
+            <!-- liste des évenement en cours -->
+            <div class="card">
+                ? ? ?
+            </div>
+        </div>
+    </div>
+</div>
+    
