60191e9919fdc1a5c6c02b958a6669789bea3a42
garnesson <pgns@laposte.net>
2016-11-30 15:23:38 +0000

    ajout de menu laterale amis + menu lateral droit . modification de la page d'accueil. ajout de contenu sur messagerie.

---
+++ pages/liste_amis_laterale.php
@@ -0,0 +1,30 @@
+        <div class="col s3">
+            <!-- menu latéral gauche page d'accueil -->
+            <div class="card-panel">
+                <span class="card-title">Mes amis</span>
+                <div class="row">
+                    <p>
+                    <div class="chip col s12">
+                        <img src="img/face1.jpg" alt="Contact Person">
+                        <a>Axel Air</a>
+                    </div>
+                    <div class="chip col s12">
+                        <img src="img/face3.jpg" alt="Contact Person">
+                        <a>Jerry Kan</a>
+                        </div>
+                    <div class="chip col s12">
+                        <img src="img/face4.jpg" alt="Contact Person">
+                        <a>Larry Bambelle</a>
+                    </div>
+                    <div class="chip col s12">
+                        <img src="img/juliette.png" alt="Contact Person">
+                        <a>Juliette Phone</a>
+                    </div>
+                    <div class="chip col s12">
+                        <img src="img/roger.png" alt="Contact Person">
+                        <a>Roger Fiesta</a>
+                    </div>
+                    </p>
+                </div>
+            </div>
+        </div>
--- pages/messagerie.php
+++ pages/messagerie.php
@@ -1,37 +1,36 @@
 <div class="container">
     <div class="row">
-        <div class="col s2">
-            <!-- menu latéral gauche page d'accueil -->
-            <div class="card-panel">
-                liste des amis
-            </div>
-        </div>
-        <div class="col s7">
+        <?php include'liste_amis_laterale.php'; ?>
+        
+        <!-- Liste des messages -->
+        <div class="col s6">
             <div class="row">
                 <div class="col s12">
                    <div class="card-panel">
-                        <!-- message pouvant etre poster par l'utilisateur -->
+                        <!-- formulaire message pouvant etre poster par l'utilisateur -->
                         <div class="input-field">
-                           <label for="textarea1">Nouveau message</label>
+                            Destinataire : 
+                        </div>
+                        
+                        <div class="input-field">
+                            <label for="textarea1">Envoyer un nouveau message</label>
                             <textarea id="textarea1" class="materialize-textarea"></textarea>
-                            
+                            <button class="btn waves-effect waves-light" type="submit" name="action">Envoyer
+                                <i class="material-icons right"></i>
+                            </button>
                         </div>
                     </div>
                 </div>
                 <div class="col s12">
                     <div class="card-panel">
                         <!--affichage des derniers message de groupes ' a voir avec barnabas-->
-                        Liste des anciens messages
+                        Discussion en cours
                             
                     </div>       
                 </div>
             </div>
         </div>
-        <div class="col s3">
-            <!-- liste des évenement en cours -->
-            <div class="card">
-                ? ? ?
-            </div>
+      <?php include'menu_laterale_droit.php';?>
         </div>
     </div>
 </div>
