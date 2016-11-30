4898edef398ff0cdbd36cdfe83ae1ad5a5ab4df4
garnesson <pgns@laposte.net>
2016-11-30 15:44:15 +0000

    ajout des amis qu'on pourrait connaitre + modif messagerie

---
+++ pages/connaissez_vous_peut_etre.php
@@ -0,0 +1,23 @@
+
+<!-- liste des évenement en cours -->
+<div class="card">
+    <div class="row">
+        <div class="col s12">
+            <center><h4 class="header">Connaissez-vous...</h4></center>
+            <!--<div class="card">
+                <span class="card-title"><center>Évènement</center></span>
+            </div>-->
+        </div>
+        <div class="col s12">
+            <div class="card">
+                <div class="card-image">
+                   
+                </div>
+                <div class="card-content">
+               texte
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</div>
