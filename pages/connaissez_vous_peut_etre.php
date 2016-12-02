0418979c69f4defc840ec98d185523ff45b189e0
garnesson <pgns@laposte.net>
2016-12-02 15:16:06 +0000

    ajout contenu

--- pages/connaissez_vous_peut_etre.php
+++ pages/connaissez_vous_peut_etre.php
@@ -12,20 +12,19 @@
                 <div class="card-content">
                     <div class="row">
 
-                        <div class="chip col s12">
+                        <div class="chip col s12 valign-wrapper">
                             <img src="img/face2.jpg" alt="Contact Person">
-                            <a>Alain Proviste</a><a><i class="material-icons right valign-wrapper">add</i></a>
+                            <a>Alain Proviste</a><a><i class="material-icons right valign tiny">add</i></a>
                         </div>
-                        <div class="chip col s12">
+                        <div class="chip col s12 valign-wrapper">
                             <img src="img/face5.jpg" alt="Contact Person">
-                            <a>Simon Strueux</a><a><i class="material-icons right valign">add</i></a>
+                            <a>Simon Strueux</a><a><i class="material-icons right valign tiny">add</i></a>
                         </div>
-                        <div class="chip col s12">
+                        <div class="chip col s12 valign-wrapper">
                             <img src="img/face6.jpg" alt="Contact Person">
-                            <a>Cara Melmou</a><a><i class="material-icons right valign">add</i></a>
+                            <a>Cara Melmou</a><a><i class="material-icons right valign tiny">add</i></a>
                         </div>
                     </div>
-                    
                     <!--<div class="chip col s12">
                         <img src="img/face1.jpg" alt="Contact Person">
                         <a>Axel Air</a>
