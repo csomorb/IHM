f94104d67d6689794f174086c989381503372bc4
garnesson <pgns@laposte.net>
2016-12-02 14:06:20 +0000

    ajout contenu

--- pages/menu_laterale_droit.php
+++ pages/menu_laterale_droit.php
@@ -1,4 +1,57 @@
 
+<<<<<<< HEAD
+<!-- liste des évenement en cours -->
+<div class="card">
+    <div class="row">
+        <div class="col s12">
+            <center><h4 class="header">Évènement</h4></center>
+            <!--<div class="card">
+                <span class="card-title"><center>Évènement</center></span>
+            </div>-->
+        </div>
+        <div class="col s12">
+            <div class="card">
+                <div class="card-image">
+                    <img src="https://scontent.xx.fbcdn.net/v/t1.0-0/p240x240/15085697_698488080302752_4043143901866369599_n.jpg?oh=a29b233cf81a3b7f1e564d93afb38810&oe=58D21DCD">
+                </div>
+                <div class="card-content">
+                    <span class="title-card-evenement-accueil">Université Grenoble Alpes</span>
+                    <p class="text-card-evenement-accueil">Quand nos basketteuses élite font le + beau "Kass" de l'Univ de Nice...70 - 51 !<br/><a>#MBLU</a> <a>#UGAvsNice</a></p>
+                    <a class="right">Voir</a><br/>
+                </div>
+            </div>
+        </div>
+        
+        <div class="col s12">
+            <div class="card">
+                <div class="card-image">
+                    <img src="http://www.univ-grenoble-alpes.fr/medias/photo/trump_1479286588628-jpg">
+                </div>
+                <div class="card-content">
+                    <span class="title-card-evenement-accueil">Université Grenoble Alpes</span>
+                    <p class="text-card-evenement-accueil">Trump Président, et après ? 45è président des USA... A J+7, tu te questionnes ? 4 experts UGA croisent ...</a></p>
+                    <a class="right">Voir</a><br/>
+                </div>
+            </div>
+        </div>
+        
+        <div class="col s12">
+            <div class="card">
+                <div class="card-image">
+                    <img src="https://scontent.xx.fbcdn.net/v/t1.0-9/14184276_10201855880394363_2286520047715810801_n.jpg?oh=b0e82242ffd041edaaef5efa44d877a5&oe=58B97333">
+                    <span class="card-title">Master WIC</span>
+                </div>
+                <div class="card-content">
+                    <span class="title-card-evenement-accueil">Master WIC <span class="grey-text darken-1-text">Promo 2016</span></span>
+                    <p class="text-card-evenement-accueil">On est au bshm ou a l'imag cette aprem ?</p>
+                    <a> 2 Réponses </a><a class="right">Voir</a>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</div>
+=======
                 <!-- liste des évenement en cours -->
                 <div class="card">
                     <div class="row">
@@ -50,3 +103,4 @@
                         
                     </div>
                 </div>
+>>>>>>> 6a5489e737d7feaf8764e66cd019bc1f28abb6c6
